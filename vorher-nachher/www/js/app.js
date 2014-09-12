6cfa146244347bc0e890919fb079650d964854b7
Michael <Michael@Vostro>
2014-09-13 00:05:19 +0200

    bevore adding plugin

--- vorher-nachher/www/js/app.js
+++ vorher-nachher/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('vorher-nachher', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -15,5 +15,19 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+    
+    // make points when click at image
+    $count_points_img1 = 1;
+    $('#img1').click(function(e) { //TODO: touch statt click
+        $('#img1').parent().append('<div id="punkt'+$count_points_img1+'" class="punkt"></div>');
+        $p = $('#punkt'+$count_points_img1);
+        $cw = $p.width() / 2;
+        $ch = $p.height() / 2;
+        $p.offset({top: e.pageY-$ch, left: e.pageX-$cw});
+        console.log('test'); // TODO: x/y-koordinaten des img ausgeben (später übergabe an opencv)
+        $count_points_img1 += 1;
+        
+    });
   });
 })
+
